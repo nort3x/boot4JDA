f1e3492b4989c8d05d6d794ceb6d76e0472a75fa
Human Ardaki <nort3x911@protonmail.com>
2022-03-19 09:22:04 +0330

    added default bean definition

--- core/src/main/java/me/nort3x/b4j/core/configurations/HelpGenerator.java
+++ core/src/main/java/me/nort3x/b4j/core/configurations/HelpGenerator.java
@@ -3,9 +3,13 @@ package me.nort3x.b4j.core.configurations;
 import me.nort3x.b4j.core.B4JContext;
 import me.nort3x.b4j.core.bots.IBot;
 import net.dv8tion.jda.api.events.Event;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class HelpGenerator {
@@ -15,12 +19,23 @@ public class HelpGenerator {
     B4JContext b4JContext;
 
     @Autowired
+    ApplicationContext applicationContext;
+
+    @PostConstruct
+    void construct() {
+        try {
+            helpBannerGenerator = applicationContext.getBean(HelpBannerGenerator.class);
+        }catch (NoSuchBeanDefinitionException e){
+            helpBannerGenerator = new DebugHelpGenerator();
+        }
+    }
+
     HelpBannerGenerator helpBannerGenerator;
 
-    public Object getHelpBanner(Event event){
+    public Object getHelpBanner(Event event) {
         IBot bot = b4JContext.getBotFromJDA(event.getJDA());
-        if(helpBannerGenerator.supportHelpForBotName(bot))
-            return helpBannerGenerator.generateHelpForBot(bot,b4JContext.getDescribableCommandsForBot(bot));
+        if (helpBannerGenerator.supportHelpForBotName(bot))
+            return helpBannerGenerator.generateHelpForBot(bot, b4JContext.getDescribableCommandsForBot(bot));
         return null;
     }
 
